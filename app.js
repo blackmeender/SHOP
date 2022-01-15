8c9cca5ab250b35a791ab8ee2a598b606b7d6882
blackmeender <same.98@mail.ru>
2022-01-15 18:12:32 +1100

    Добавил корзину

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-let buttons = document.querySelectorAll('.btn-overlay')
+let buttons = document.querySelectorAll('.btn-overlay[id]')
 buttons.forEach(function (button) {
     button.addEventListener('click', addProduct)
 })
@@ -14,6 +14,7 @@ function productInCart(productId) {
     increaceProductsCount()
     addToObject(productId)
     renderInCart(productId)
+    calculateAndRenderTotalCartSum()
 }
 
 function increaceProductsCount() {
@@ -39,7 +40,7 @@ openCart.addEventListener('click', function () {
 })
 
 function renderInCart(productId) {
-    let productExist = document.querySelector('.product-count[id="${productId}"]');
+    let productExist = document.querySelector(`.productCount[id="${productId}"]`);
     if (productExist) {
         increaceProductCount(productId)
         recalculateSum(productId)
@@ -51,31 +52,29 @@ function renderInCart(productId) {
 function newRenderInCart(productId) {
     let productRow = `
         <div class="cart-row">
-            <div>${nameOfCard}</div>
+            <div>${productArr[productId].name}</div>
             <div>
                 <span class="productCount" id="${productId}">1</span> шт.
             </div>
-            <div>$${price}</div>
+            <div>$${productArr[productId].priceOfCard}</div>
             <div>
-                $<span class="productTotalRow" id="${productId}">${price}</span>
+                $<span class="productTotalRow" id="${productId}">${productArr[productId].priceOfCard}</span>
             </div>
         </div>
     `
     let cartTotalEl = document.querySelector('.cart-total')
     cartTotalEl.insertAdjacentHTML("beforebegin", productRow)
 }
-
-let pp = document.querySelector('.number')
-let f = pp.innerHTML
-console.log(f)
-
-let price = document.querySelector('.number')
-let a = price.innerHTML
-console.log(a)
-let p = document.querySelector('card-title')
-let nameOfCard = p.innerHTML
-console.log(nameOfCard)
-
+let productArr = []
+let productCards = document.querySelectorAll('.product-card')
+productCards.forEach(function (productCard) {
+    let name = productCard.querySelector('.card-title').innerText
+    let priceOfCard = Number(productCard.querySelector('.number').innerText)
+    productArr.push({
+        name,
+        priceOfCard
+    })
+})
 
 
 
@@ -83,12 +82,21 @@ console.log(nameOfCard)
 
 function increaceProductCount(productId) {
     let productCountEl = document.querySelector(`.productCount[id="${productId}"]`)
-    productCountEl.textContent++;
+    productCountEl.textContent++
 }
 
-function recalculateSumForProduct(productId) {
-    const productTotalRowEl = document.querySelector(`.productTotalRow[id="${productId}"]`);
-    let totalPriceForRow = (basket[productId] * products[productId].price).toFixed(2);
-    productTotalRowEl.textContent = totalPriceForRow;
+function recalculateSum(productId) {
+    const productTotalRowEl = document.querySelector(`.productTotalRow[id="${productId}"]`)
+    let totalPriceForRow = (cart[productId] * productArr[productId].priceOfCard)
+    productTotalRowEl.textContent = totalPriceForRow
 }
 
+function calculateAndRenderTotalCartSum() {
+    let totalSum = 0
+    for (let productId in cart) {
+        totalSum += cart[productId] * productArr[productId].priceOfCard
+    }
+    cartTotalValueEl.innerText = Number(totalSum)
+}
+
+const cartTotalValueEl = document.querySelector('.total-value')
